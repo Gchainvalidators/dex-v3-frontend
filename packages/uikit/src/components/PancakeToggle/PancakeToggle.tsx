6a4672ee1ae95c5acf51df880ee631a2c2e99ace
Gchainvalidators <admin@genesys.network>
2023-11-21 10:55:59 +0700

    change image titi

--- packages/uikit/src/components/PancakeToggle/PancakeToggle.tsx
+++ packages/uikit/src/components/PancakeToggle/PancakeToggle.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PancakeStack, PancakeInput, PancakeLabel } from "./StyledPancakeToggle";
 import { PancakeToggleProps, scales } from "./types";
+import { Titi } from "../Titi";
 
 const PancakeToggle: React.FC<React.PropsWithChildren<PancakeToggleProps>> = ({
   checked,
@@ -11,10 +12,11 @@ const PancakeToggle: React.FC<React.PropsWithChildren<PancakeToggleProps>> = ({
     <PancakeInput id={props.id || "pancake-toggle"} scale={scale} type="checkbox" checked={checked} {...props} />
     <PancakeLabel scale={scale} checked={checked} htmlFor={props.id || "pancake-toggle"}>
       <div className="pancakes">
+        <Titi width={'32px'} height={'32px'} />
+        {/* <div className="pancake" />
         <div className="pancake" />
         <div className="pancake" />
-        <div className="pancake" />
-        <div className="butter" />
+        <div className="butter" /> */}
       </div>
     </PancakeLabel>
   </PancakeStack>
