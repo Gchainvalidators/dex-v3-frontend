9f3a55cfe67d074f07bafd10061ed73c8293b95b
Gchainvalidators <admin@genesys.network>
2023-12-04 13:31:38 +0700

    fix text

--- apps/web/src/utils/transactionErrorToUserReadableMessage.ts
+++ apps/web/src/utils/transactionErrorToUserReadableMessage.ts
@@ -24,18 +24,18 @@ export function transactionErrorToUserReadableMessage(error: any, t: TranslateFu
 
   const formatErrorMessage = (message: string) => [message, `(${reason})`].join(' ')
   switch (reason) {
-    case 'PancakeRouter: EXPIRED':
+    case 'BluelotusdaoRouter: EXPIRED':
       return formatErrorMessage(
         t(
           'The transaction could not be sent because the deadline has passed. Please check that your transaction deadline is not too low.',
         ),
       )
-    case 'PancakeRouter: INSUFFICIENT_OUTPUT_AMOUNT':
-    case 'PancakeRouter: EXCESSIVE_INPUT_AMOUNT':
-    case 'PancakeRouter: INSUFFICIENT_A_AMOUNT':
-    case 'PancakeRouter: INSUFFICIENT_B_AMOUNT':
+    case 'BluelotusdaoRouter: INSUFFICIENT_OUTPUT_AMOUNT':
+    case 'BluelotusdaoRouter: EXCESSIVE_INPUT_AMOUNT':
+    case 'BluelotusdaoRouter: INSUFFICIENT_A_AMOUNT':
+    case 'BluelotusdaoRouter: INSUFFICIENT_B_AMOUNT':
     case 'swapMulti: incorrect user balance':
-    case 'Pancake: K':
+    case 'Bluelotusdao: K':
       return formatErrorMessage(
         t(
           'This transaction will not succeed either due to price movement or fee on transfer. Try increasing your slippage tolerance.',
@@ -43,7 +43,7 @@ export function transactionErrorToUserReadableMessage(error: any, t: TranslateFu
       )
     case 'TransferHelper: TRANSFER_FROM_FAILED':
       return formatErrorMessage(t('The input token cannot be transferred. There may be an issue with the input token.'))
-    case 'Pancake: TRANSFER_FAILED':
+    case 'Bluelotusdao: TRANSFER_FAILED':
       return formatErrorMessage(
         t('The output token cannot be transferred. There may be an issue with the output token.'),
       )
